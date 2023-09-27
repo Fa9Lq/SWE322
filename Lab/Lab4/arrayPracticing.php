ddedf2fdf68c7df6b40ca4a9984bac6b963e153a
Faisal Alshuwaier <64235100+Fa9Lq@users.noreply.github.com>
2023-09-27 15:40:18 +0300

    Update arrayPracticing.php

--- Lab/Lab4/arrayPracticing.php
+++ Lab/Lab4/arrayPracticing.php
@@ -1,15 +1,31 @@
-<?php 
-
 <!DOCTYPE html>
 <html>
 <body>
-
 <?php
-echo "My first PHP script!";
+
+$a = array( "p1" => "orange",
+			"p2" => "banana",
+            "p3" => "apple");
+    
+echo count ($a);
+echo "<br>";
+
+echo array_values($a)[0];
+echo "<br>";
+
+
+foreach($a as $code => $value ){
+
+echo $value;
+echo "<br>";
+
+}
+foreach($a as $code => $value ){
+if($code == "p2"){
+echo $value;
+}
+}
 ?>
 
 </body>
 </html>
-
-
-?>
